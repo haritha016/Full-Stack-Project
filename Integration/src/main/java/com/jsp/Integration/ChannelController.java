20f9ca3a1fd07c8028d7f4d22d2f9b98534c6398
Harita <rharitaa@gmail.com>
2025-04-13 01:57:19 +0530

    new changes

--- Integration/src/main/java/com/jsp/Integration/ChannelController.java
+++ Integration/src/main/java/com/jsp/Integration/ChannelController.java
@@ -12,7 +12,7 @@ public class ChannelController {
 	@PostMapping(value = "/sendSms")
 	public String sendSms(@RequestBody Map<String ,String> data) {
 		System.out.println(data.get("smsContent"));
-
+		System.out.println("hello new changes");
 		return "sms send successfully" ;
 
 	}
